3b603685b34598a2c9bbd4446a2c6cd58d218410
bananazigzags <bananazigzags@gmail.com>
2021-08-04 21:01:43 +0700

    built the buttons without functionality

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+    return num1 * num2;
+};
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+const operate = function(operator, num1, num2) {
+    return operator(num1, num2);
+};
+
+console.log(operate(multiply, 5, 7));
